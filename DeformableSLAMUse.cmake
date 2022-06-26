8998c67e27e29e0bce7f3dbf25a1b3acab8e5306
shu-gong <shugong.live@gmail.com>
2022-06-26 21:00:13 +0800

    modify cmake

---
+++ DeformableSLAMUse.cmake
@@ -0,0 +1,10 @@
+#------------------------------------------------------------------------------
+# DeformableSLAM_LIBRARIES variable
+#------------------------------------------------------------------------------
+set (DeformableSLAM_LIBRARIES ${DeformableSLAM_LIBRARY})
+
+#------------------------------------------------------------------------------
+# Where to look for includes and libraries
+#------------------------------------------------------------------------------
+include_directories( ${DeformableSLAM_INCLUDE_DIRS})
+link_directories (${DeformableSLAM_LIBRARY_DIRS})
